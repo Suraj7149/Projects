03a7f85a722cba560485fbf21fe95bebf1a6a6c6
Suraj7149 <santoshsuraj7149@gmail.com>
2021-05-20 02:10:09 +0530

    files managed

---
+++ C learning/sumofnumber.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main(){
+    int a, b, sum;
+
+    printf("\nEnter the value of a and b: \n");
+    scanf("%d, %d", &a, &b);
+
+    sum = a+b;
+
+    printf("The sum of a and b is:  %d",sum);
+    return 0;
+}
