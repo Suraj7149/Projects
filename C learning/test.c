4181cf7525901054c9182bf287689b35863dbc04
Suraj7149 <santoshsuraj7149@gmail.com>
2021-05-26 23:14:01 +0530

    commited

--- C learning/test.c
+++ C learning/test.c
@@ -1,110 +1,39 @@
 #include<stdio.h>
-#include<string.h>
-
-void literals(){
-    printf("\nhttps://www.geeksforgeeks.org/literals-in-python/ \n\n");
-
-} 
-
-void identifiers(){
-    printf("\nA Python identifier can be a combination of lowercase/ uppercase\n"
-    "letters, digits, or an underscore. The following characters are valid:\n"
-
-    "Lowercase letters (a to z)\n"
-    "Uppercase letters (A to Z)\n"
-    "Digits (0 to 9)\n"
-    "Underscore (_)\n"
-
-    "Some valid names are:\n"
-        "myVar\n"
-        "var_3\n"
-        "this_works_to\n"
-
-    "b. An identifier cannot begin with a digit.\n"
-    
-   "Some valid names:\n"
-        "_9lives\n"
-        "lives9\n"
-        
-    "An invalid name:\n"
-        "9lives\n\n");
-
-}
-
-void keywords(){
-    printf("\nKeywords in python are:- \n"
-        "False\n"
-        "None\n"
-        "True\n"
-        "__peg_parser__\n"
-        "and\n"
-        "as\n"
-        "assert\n"
-        "async\n"
-        "await\n"
-        "break\n"
-        "class\n"
-        "continue\n"
-        "def\n"
-        "del\n"
-        "elif\n"
-        "else\n"
-        "except\n"
-        "finally\n"
-        "for\n"
-        "from\n"
-        "global\n"
-        "if\n"
-        "import\n"
-        "in\n"
-        "is\n"
-        "lambda\n"
-        "nonlocal\n"
-        "not\n"
-        "or\n"
-        "pass\n"
-        "raise\n"
-        "return\n"
-        "try\n"
-        "while\n"
-        "with\n"
-        "yield\n\n");
-
-}
+#include<stdlib.h>
+#include<time.h>
 
 int main(){
+    
+    int num_of_guess = 1, guess, number;
+    srand(time(0));
+    number = rand()%100 + 1;
 
-   int a = 0;
-    char input[50];
+    while (number)
+    {
+        printf("Enter the number:- ");
+        scanf("%d",&guess);
 
-    while(a == 0){
-        printf(">>>");
-        scanf("%s", &input);
-
-        if (strcmp(input, "exit") == 0){
-            a++;
+        if (guess == number){
+            printf("You guessed the correct Number.\n");
+            printf("The number of guess are %d\n", num_of_guess);
             break;
-            
-        }else if(strcmp(input, "help") == 0){
-            printf("Commands:-\n\t- help\n\t- indentifiers\n\t- literals\n\t- keywords\n\t- about\n\t- exit\n");
-
-        }else if(strcmp(input, "about") == 0){
-            printf("made by Godfather.\ncmd based program\n");
-
         }
-        else if(strcmp(input, "literals") == 0){
-            literals();
-
-        }else if(strcmp(input, "identifiers") == 0){
-            identifiers();
-
-        }else if(strcmp(input, "keywords") == 0){
-            keywords();
-
-        }
-        else{
+        else if(guess > number){
+            printf("Your number is higher.\n");
+            printf("The number of guess are %d\n", num_of_guess);
+            num_of_guess++;
             continue;
+        }else if(guess < number){
+            printf("Your number is lower.\n");
+            printf("The number of guess are %d\n", num_of_guess);
+            num_of_guess++;
+            continue;
+        }else{
+            printf("wrong input!!\n");
+            printf("The number of guess are %d\n", num_of_guess);
         }
-    } 
-
+    }
+    
+    
+    return 0;
 }
