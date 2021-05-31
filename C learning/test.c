e5c9389c0f43d82a3f7fa1b07664c09bc23fbb84
Suraj7149 <santoshsuraj7149@gmail.com>
2021-05-31 15:29:11 +0530

    commited

--- C learning/test.c
+++ C learning/test.c
@@ -1,39 +1,17 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+void display(){
+    printf("fuction running.\n");
+}
 
 int main(){
     
-    int num_of_guess = 1, guess, number;
-    srand(time(0));
-    number = rand()%100 + 1;
+    printf("into main.\n");
+    printf("calling fuction.\n");
 
-    while (number)
-    {
-        printf("Enter the number:- ");
-        scanf("%d",&guess);
+    display();
 
-        if (guess == number){
-            printf("You guessed the correct Number.\n");
-            printf("The number of guess are %d\n", num_of_guess);
-            break;
-        }
-        else if(guess > number){
-            printf("Your number is higher.\n");
-            printf("The number of guess are %d\n", num_of_guess);
-            num_of_guess++;
-            continue;
-        }else if(guess < number){
-            printf("Your number is lower.\n");
-            printf("The number of guess are %d\n", num_of_guess);
-            num_of_guess++;
-            continue;
-        }else{
-            printf("wrong input!!\n");
-            printf("The number of guess are %d\n", num_of_guess);
-        }
-    }
-    
-    
+    printf("fuction over.\n");
+    printf("2. main end.\n");  
     return 0;
+    printf("1. main end.\n");
 }
