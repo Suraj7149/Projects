3d47ec915bef25c5881041c2012ccb4d35726be8
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-07 00:46:02 +0530

    commited:

--- python/notepad.py
+++ python/notepad.py
@@ -10,10 +10,10 @@ try:
     else:
         os.mkdir("Notepad")
 except FileNotFoundError:
-    if os.path.exists("C:\\Notepad\\"):
+    if os.path.exists("C:/Notepad"):
         pass
     else:
-        os.mkdir("C:\\Notepad\\")
+        os.mkdir("C:/Notepad")
 
 # creating a function to clear
 def clear():
@@ -24,38 +24,58 @@ def clear():
 # func. to save the text before clearing or quitting
 def save_file():
     if os.path.exists("C:\\Notepad\\") or os.path.exists("Notepad"):
-        file = open("Notepad/" + my_text.get(1.0, 1.5)+ ".txt", "w")
+        file = open("Notepad/" + str(datetime.date.today())+ ".txt", "w")
         file.writelines(my_text.get(1.0, END))
         file.close()
         my_text.delete(1.0, END)
     else:
 
         try:
-            if os.path.exists("Notepad\\"):
+            if os.path.exists("Notepad"):
                 pass
             else:
-                os.mkdir("Notepad\\")
+                os.mkdir("Notepad")
         except FileNotFoundError:
-            if os.path.exists("C:\\Notepad\\"):
+            if os.path.exists("C:/Notepad"):
                 pass
             else:
-                os.mkdir("C:\\Notepad\\")
+                os.mkdir("C:/Notepad")
+
+
+def open_file():
+    pass
+
+
+def change_bg():
+    pass
+
+
+def change_font():
+    pass 
+
+def bold():
+    pass
+
+def italic():
+    pass
 
 
 def run():
     global root
     global my_text
+    
+
     # initializing window 
     root = Tk()
     root.title("Personal Diary")
-    root.geometry("700x535")
+    root.geometry("900x550")
 
     # displaying year-month-date
     Label(root, text="Today is:- " + str(datetime.date.today())).pack()
 
     # generating text box
-    my_text = Text(root, width=60, height=20, font=("Ubuntu", 14))
-    my_text.pack(pady=20)
+    my_text = Text(root, width=80, height=20, font=("Ubuntu", 14))
+    my_text.pack(pady=10, padx=15)
 
     # frame for button as pack() and grid function don't work in one window
     button_frame = Frame(root)
@@ -64,15 +84,15 @@ def run():
     # clear text button
     clear_button = Button(button_frame, text="Clear All", command=clear)
     # arranging
-    clear_button.grid(row=0, column=0)
+    clear_button.grid(column=0)
 
     # Save button
     get_text_button = Button(button_frame, text="Save", command=save_file)
-    get_text_button.grid(row=0, column=1, padx=10)
+    get_text_button.grid(column=1, padx=10)
 
     # quit button
     quit_button = Button(button_frame, text="Quit", command=root.destroy)
-    quit_button.grid(row=0, column=2, padx=10)
+    quit_button.grid(column=2, padx=10)
 
     root.resizable(0, 0)
     # looping to display the window
