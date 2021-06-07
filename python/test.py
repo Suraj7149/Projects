f6d4b53da3704b0d86ab7f791c37e010fc3904e0
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-07 18:09:07 +0530

    commited

--- python/test.py
+++ python/test.py
@@ -1,3 +1,4 @@
+from notepad import open_file
 from tkinter import * 
 from tkinter import filedialog
 from tkinter import font
@@ -9,16 +10,39 @@ root = Tk()
 root.geometry(f"{w}x{h}")
 root.title("Notepad")
 
+def new_file():
+    my_text.delete("1.0", END)
+    root.title("New File - Notepad!")
+    status_bar.config(text="New File - Unsaved     ")
+
+def open_file():
+    my_text.delete("1.0", END)
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*'),
+        
+    )
+    # show the open file dialog
+    f = filedialog.askopenfile(initialdir="/home/suraj/Desktop", title="Open files", filetypes=filetypes)
+    # read the text file and show its content on the Text
+    name = f
+    status_bar.config(text=name)
+    print(name)
+    my_text.insert('1.0', f.readlines())
 
 text_font = Font(family="Times New Roman", size=14, slant="italic")
 Button_font = Font(family="Ubuntu", size=12, weight="bold")
 
+status_bar = Label(root, text="Ready    ", font=Button_font, anchor=E)
+status_bar.pack(fill=X,side=BOTTOM,)
+
 
 menubar = Menu(root, fg ="white", bg="black", font=Button_font)
 file = Menu(menubar, tearoff=0, fg ="white", bg="black", font=Button_font)
+file.add_command(label="New", command=new_file)  
 file.add_command(label="Save")   
 file.add_command(label="Save as..")
-file.add_command(label="Open")
+file.add_command(label="Open", command=open_file)
 file.add_command(label="Recent..")
 file.add_command(label="Close", command=root.destroy)
 file.add_separator()
@@ -51,5 +75,5 @@ my_text.pack(side=LEFT, fill="both")
 
 text_scroll.config(command=my_text.yview)
 root.config(menu=menubar)
-root.resizable(0,0)
+# root.resizable(0,0)
 root.mainloop()
