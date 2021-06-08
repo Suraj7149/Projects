53f5212b831b20c929dee9aeac5d4dab2fc597fc
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-08 13:59:55 +0530

    commited

--- python/notepad.py
+++ python/notepad.py
@@ -1,6 +1,3 @@
-from os import path
-
-from easygui.boxes.fileopen_box import fileopenbox
 from tkinter import * 
 from tkinter import filedialog
 from tkinter import font
@@ -9,9 +6,52 @@ import easygui
 
 w = 800
 h = 600
+filetype = (
+    ("Text Files", "*.txt"),
+    ("All Files", "*.*")
+)
+file_path = None
 root = Tk()
 root.geometry(f"{w}x{h}")
 root.title("Notepad")
+
+def get_name(name):
+    string1 = ''
+    for i in range(1, len(name)):
+        if name[-i] == "/":
+            break
+        else:
+            string1 = string1+(name[-i])
+            continue
+    
+    return string1[::-1]
+
+def  save_file():
+    text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="home/suraj/Desktop/Projects/", filetypes=filetype)
+    if text_file:
+        file_path = text_file
+        name = text_file
+        name = get_name(text_file)
+        status_bar.config(text=f'Saved: {text_file}')
+        root.title(f'{name}')
+
+        text_file = open(text_file, "w")
+        text_file.write(my_text.get(1.0, END))
+        text_file.close()
+
+
+def save():
+    global file_path
+    if file_path:
+        text_file = open(file_path, "w")
+        text_file.write(my_text.get(1.0, END))
+        text_file.close()
+        root.title(get_name(file_path))
+        status_bar.config(text=f'Saved: {file_path}')
+    else:
+        save_file()
+
+
 
 def full():
     w = root.winfo_screenwidth
@@ -24,30 +64,23 @@ def new_file():
     my_text.delete("1.0", END)
     root.title("New File - Notepad!")
     status_bar.config(text="New File - Unsaved     ")
+    file_path = False
+
 
 def open_file():
-    my_text.delete("1.0", END)
-    filetypes = (
-        ('text files', '*.txt'),
-        ('All files', '*.*'),
-        
-    )
-    # show the open file dialog
-    path = easygui.fileopenbox()
-    # read the text file and show its content on the Text
-    f = open(path, "r")
-    status_bar.config(text=path)
-    
-    string1 = " " 
-    for i in range(1, len(path)):
-        if path[-i] == "/":
-            break
-        else:
-            string1 = string1+(path[-i])
-            continue
-    root.title(string1[::-1]+"(Read-Only-Mode)")
+    global file_path
 
-    my_text.insert('1.0', f.readlines())
+    my_text.delete("1.0", END)
+    
+    # show the open file dialog
+    file_path = easygui.fileopenbox()
+
+    # read the text file and show its content on the Text
+    f = open(file_path, "r")
+    status_bar.config(text=file_path)
+    root.title(get_name(file_path)+"(Read-Only-Mode)")
+
+    my_text.insert('1.0', f.read())
     f.close()
 
 text_font = Font(family="Times New Roman", size=14, slant="italic")
@@ -60,8 +93,8 @@ status_bar.pack(fill=X,side=BOTTOM,)
 menubar = Menu(root, fg ="white", bg="black", font=Button_font)
 file = Menu(menubar, tearoff=0, fg ="white", bg="black", font=Button_font)
 file.add_command(label="New", command=new_file)  
-file.add_command(label="Save")   
-file.add_command(label="Save as..")
+file.add_command(label="Save", command=save)   
+file.add_command(label="Save as..", command=save_file)
 file.add_command(label="Open", command=open_file)
 file.add_command(label="Recent..")
 file.add_command(label="Close", command=root.destroy)
