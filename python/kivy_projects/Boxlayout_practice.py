ce76a8408d7a82582d0826bfd03633b46f104fe5
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-12 00:06:47 +0530

    learned how to add image in background

--- python/kivy_projects/Boxlayout_practice.py
+++ python/kivy_projects/Boxlayout_practice.py
@@ -1,20 +1,22 @@
 import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
-Builder.load_file("box1.kv")
+Builder.load_file("Float_Layout.kv")
 
 class MyLayout(Widget):
+    
+    Window.size = (800,600)
     pass
 
 
-class BoxApp(App):
+class Float_LayoutApp(App):
     def build(self):
+        
         return MyLayout()
 
 
 if __name__ == "__main__":
-    BoxApp().run()
+    Float_LayoutApp().run()
