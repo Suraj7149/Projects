4ae8db240b44e4068d90f22399cfe82a5020b16f
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-19 16:45:16 +0530

    commited

--- C learning/test.c
+++ C learning/test.c
@@ -1,6 +1,9 @@
 # include<stdio.h>
 
-void main(){
-    int i = 1;
-    printf("%d",i);
+int main(){
+    int a = 1 , b = 1, m=1,n = 1, c = 1, d = 2,j;
+    j= (a * 4*(c*c))-d;
+
+    printf("%i\n",(j/(m+n)));
+    return 0;
 }
