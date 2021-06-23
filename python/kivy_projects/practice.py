da4997237c0a8d084a59b57b9ec630bab944a9a6
Suraj7149 <santoshsuraj7149@gmail.com>
2021-06-24 00:42:59 +0530

    added accordian, check_boxes and carousel

--- python/kivy_projects/practice.py
+++ python/kivy_projects/practice.py
@@ -1,22 +1,40 @@
+from os import terminal_size
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 
 Window.size = (800,600)
-Builder.load_file("slider.kv")
+Builder.load_file("check_boxes.kv")
 
 class MyLayout(Widget):
-    def selected(self, filename):
-        try:
-            self.ids.my_image.source = filename[0]
-            #print(filename[0])
-        except:
-            pass
+    checks = []
+    
+    def checkbox_click(self, instance, value, toppings):
+        if value == True:
+            MyLayout.checks.append(toppings)
+            tops = ''
+            for x in MyLayout.checks:
+                tops = tops +" "+ x
+            self.ids.output_label.text = f"You selected: {tops}"
+        else:
+            MyLayout.checks.remove(toppings)
+            tops = ''
+            for x in MyLayout.checks:
+                tops = tops +" "+ x
+            self.ids.output_label.text = f"You selected: {tops}"
 
-    def slide_it(self, *args):
-        self.slide_text.text = str(int(args[1]))
-        self.slide_text.font_size = str(int(args[1]) * 5)
+    # def selected(self, filename):
+    #     try:
+    #         self.ids.my_image.source = filename[0]
+    #         #print(filename[0])
+    #     except:
+    #         pass
+
+    # def slide_it(self, *args):
+    #     self.slide_text.text = str(int(args[1]))
+    #     self.slide_text.font_size = str(int(args[1]) * 5)
+    pass
 
 class PracticeApp(App):
     def build(self):
